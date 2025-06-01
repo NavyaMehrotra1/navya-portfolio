5018838b891d4af81ec89f81d88dbd7302dfd15f
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-06-01 14:34:19 +0530

    about section

---
+++ src/components/about.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return(
+        <section>
+            <h2>About Me</h2>
+            <p>Hi, I'm Navya Mehrotra, a student at Johns Hopkins University studying Computer Science and Economics.</p>
+        </section>
+    ); 
+}
+
+export default About; 
